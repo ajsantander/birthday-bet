b0aa31c96c6fccbe8c0000bfb83612b3db82b525
Alejandro Santander <palebluedot@gmail.com>
2017-06-24 14:25:48 -0300

    Update gas

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,5 +5,5 @@ module.exports = function(deployer) {
   let unitBetWei = web3.toWei(0.5, 'ether');
   let lastDayToBetUnix = Math.floor(new Date('July 1, 2017').getTime() / 1000);
   let debugMode = true;
-  deployer.deploy(BetOnDate, unitBetWei, lastDayToBetUnix, debugMode, {gas:1000000});
+  deployer.deploy(BetOnDate, unitBetWei, lastDayToBetUnix, debugMode, {gas: 1027206});
 };
